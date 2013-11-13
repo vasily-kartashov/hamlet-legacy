7261ebdb817aa3d43a2388f738b57ac36b18692a
Vasily Kartashov <kartashov.vasily@googlemail.com>
2013-11-13 14:14:52 +0000

    oh, the spaces

--- public/js/Application.ts
+++ public/js/Application.ts
@@ -1,8 +1,7 @@
 /**
  * Created by danny on 13/11/2013.
+ * De-dannyfied by vasily on 13/11/2013
  */
-
-
 ///<reference path="Environment"/>
 ///<reference path="vendor/jquery-2.0.3.d"/>
 ///<reference path="vendor/facebook.d"/>
@@ -10,17 +9,12 @@
 ///<reference path="Views"/>
 
 class Application {
-
-
-    constructor( private environment : Environment)
-    {
-        this.loadFacebookSDK(()=> this.initFacebook());
-        $(document).ready(()=> this.onReady());
+    constructor(private environment: Environment) {
+        this.loadFacebookSDK(() => this.initFacebook());
+        $(document).ready(() => this.onReady());
     }
 
-
-    private loadFacebookSDK(callback : ()=>void)
-    {
+    private loadFacebookSDK(callback : () => void) {
         window.fbAsyncInit = callback;
         (function(d){
             var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
@@ -31,13 +25,9 @@ class Application {
 
     }
 
-    private onReady()
-    {
+    private onReady() {}
 
-    }
-
-    private initFacebook()
-    {
+    private initFacebook() {
         FB.init({
             appId: this.environment.getFacebookAppId(),
             status: true,
@@ -52,7 +42,4 @@ class Application {
             }
         });
     }
-
-
-
 }
