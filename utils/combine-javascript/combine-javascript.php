c3ca292d1cbdfbbc9ef9fac02a206e9facdd20ca
vasily-kartashov <Saratov2001>
2013-12-16 09:51:46 +0000

    minor updates

--- utils/combine-javascript/combine-javascript.php
+++ utils/combine-javascript/combine-javascript.php
@@ -2,14 +2,14 @@
 require_once(__DIR__ . '/../../library/runtime.php');
 require_once(__DIR__ . '/../../library/build.php');
 
-$dirPath = realpath(__DIR__ . '/../../public/js');
+$basePath = realpath(__DIR__ . '/../../public/js');
 
-$parser = new \core\typescript\Parser();
-$dependencies = $parser->collectDependencies($dirPath);
+$parser = new \core\typescript\Parser($basePath);
+$dependencies = $parser->collectDependencies();
 
 $content = '';
 foreach ($dependencies as $dependency) {
-    $content .= file_get_contents($dirPath . '/' . $dependency);
+    $content .= file_get_contents($basePath . '/' . $dependency);
 }
 
-file_put_contents($dirPath . '/all.js', $content);
+file_put_contents($basePath . '/all.js', $content);
