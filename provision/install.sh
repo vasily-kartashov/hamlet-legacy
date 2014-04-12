7e0bc54109f021743a84c996c5c666b2192308b4
Vasily Kartashov <info@kartashov.com>
2014-04-12 20:07:20 +0100

    upgrade the provision to php 5.4 and apache 2.4.9

--- provision/install.sh
+++ provision/install.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 
 sudo apt-get update
+sudo apt-get install python-software-properties -y
+sudo add-apt-repository ppa:ondrej/php5 -y
+sudo apt-get update
 
-sudo apt-get -y install apache2 php5 php5-cli php-pear php5-mysql php5-curl unzip make php5-dev libpcre3-dev memcached php5-memcached php5-sqlite jpegtran -y
-sudo pecl install xdebug
-printf "\n" | sudo pecl install apc
-sudo apt-get install php5-gd -y
+sudo apt-get install apache2 php5 php5-cli php-pear php5-mysql php5-curl unzip make php5-dev php-apc php5-xdebug \
+             libpcre3-dev memcached php5-memcached php5-sqlite libjpeg-progs libapache2-mod-php5 -y
 
-sudo cp /vagrant/provision/config/dev /etc/apache2/sites-available
+sudo cp /vagrant/provision/config/dev.conf /etc/apache2/sites-available
+sudo cp /vagrant/provision/config/servername.conf /etc/apache2/conf-available
 sudo cp /vagrant/provision/config/php.ini /etc/php5/apache2
 sudo cp /vagrant/provision/config/php.ini /etc/php5/cli
-sudo cp /vagrant/provision/config/apc.ini /etc/php5/apache2/conf.d
 sudo cp /vagrant/provision/config/xdebug.ini /etc/php5/apache2/conf.d
 sudo cp /vagrant/provision/config/hosts /etc
 
-php /vagrant/desktop/utils/create-library-loader.php
-php /vagrant/desktop/utils/google-drive-import.php
-php /vagrant/mobile/utils/create-library-loader.php
-php /vagrant/mobile/utils/google-drive-import.php
+php /vagrant/utils/create-library-loader/create-library-loader.php
 
+sudo a2enconf servername
 sudo a2dismod negotiation
-sudo a2dissite default
+sudo a2dissite 000-default
 sudo a2ensite dev
 sudo a2enmod rewrite
 sudo a2enmod expires
