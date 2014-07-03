6e138453a8cd844b78c6f9efd55818a4c4dc2fb6
Vasily Kartashov <kartashov.vasily@googlemail.com>
2014-07-03 14:32:15 +0100

    Delete index.php

--- public/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-http_response_code(500); // Unexpected errors killing the php module should have the right response code set
-
-require(__DIR__ . '/../library/runtime.php');
-$application = new \application\FrontendApplication();
-$request = new core\request\WebRequest();
-$response = $application->run($request);
-$application->output($request, $response);
